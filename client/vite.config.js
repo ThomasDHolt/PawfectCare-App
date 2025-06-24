b8543488c6f7fa865d6a6cd820d7a336afc53c68
SanguiniusDeathgod <cucumella0@gmail.com>
2025-06-25 00:27:22 +0100

    finally manage to solve nesting

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,9 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about/index.html"),
+        contact: resolve(__dirname, "contact/index.html"),
+        home: resolve(__dirname, "home/index.html"),
         login: resolve(__dirname, "login/index.html"),
         signup: resolve(__dirname, "signup/index.html"),
       },
