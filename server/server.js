37d74ef441e5816e87910759ce6542bfd87ec65b
ThomasDHolt <tholt9111@gmail.com>
2025-06-24 14:40:38 +0100

    Create basic GET routes for each of the DB tables.

--- server/server.js
+++ server/server.js
@@ -10,10 +10,44 @@ app.use(express.json());
 
 dotenv.config();
 
+const db = new pg.Pool({
+    connectionString: process.env.DB_CONN
+});
+
 app.get("/", (req, res) => {
     res.json("On the Pet Sitter API root route");
 });
 
-app.listen("8080", () => {
+app.get("/accounts", async (req, res) => {
+    const result = await db.query('SELECT * FROM account');
+
+    res.json(result.rows);
+});
+
+app.get("/personalInfo", async (req, res) => {
+    const result = await db.query('SELECT * FROM personalinfo');
+
+    res.json(result.rows);
+});
+
+app.get("/petOwners", async (req, res) => {
+    const result = await db.query('SELECT * FROM petowner');
+
+    res.json(result.rows);
+});
+
+app.get("/petSitters", async (req, res) => {
+    const result = await db.query('SELECT * FROM petsitter');
+
+    res.json(result.rows);
+});
+
+app.get("/petInfo", async (req, res) => {
+    const result = await db.query('SELECT * FROM petinfo');
+
+    res.json(result.rows);
+});
+
+app.listen("4974", () => {
     console.log("Server running...");
 });
