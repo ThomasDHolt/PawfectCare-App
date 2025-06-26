3477a5286bacbb473d03f1bcaa36770a11ae36ee
ThomasDHolt <tholt9111@gmail.com>
2025-06-26 16:21:47 +0100

    Create barebones DOM logic for petSitter page

--- client/petSitters/TomsFetchLogic.js
+++
@@ -1,6 +0,0 @@
-async function FetchPetSitters()
-{
-    const res = await fetch("https://week5-petsitterapp-server.onrender.com/accounts/getByType/petSitter");
-    const accounts = await res.json();
-    return accounts;
-}
